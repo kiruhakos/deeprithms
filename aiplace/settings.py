b62a2f6d846a2f1a0b8ab60526423518ff8e1aa0
Kyrylo Kostiuk <110406944+kiruhakos@users.noreply.github.com>
2023-07-19 15:04:32 +0300

    Changed settings.py

--- aiplace/settings.py
+++ aiplace/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'deeprithms.onrender.com']
 
@@ -348,50 +348,42 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-
-
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'filters': {
-#         'require_debug_false': {
-#             '()': 'django.utils.log.RequireDebugFalse'
-#         }
-#     },
-#     'formatters': {
-#         'verbose': {
-#             'format': '[contactor] %(levelname)s %(asctime)s %(message)s'
-#         },
-#     },
-#     'handlers': {
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#         },
-#         'syslog':{
-#             'level':'INFO',
-#             'class': 'logging.handlers.SysLogHandler',
-#             'facility': SysLogHandler.LOG_LOCAL2,
-#             'address': '/dev/log',
-#             'formatter': 'verbose',
-#         },
-#         'mail_admins': {
-#             'level': 'WARNING',
-#             'filters': ['require_debug_false'],
-#             'class': 'django.utils.log.AdminEmailHandler',
-#         },
-#         'sentry': {
-#             'level': 'ERROR',
-#             'filters': ['require_debug_false'],
-#             'class': 'sentry_sdk.integrations.django.DjangoIntegration',
-#         },
-#     },
-#     'loggers': {
-#         '': {
-#             'handlers': ['console', 'syslog', 'mail_admins', 'sentry'],
-#             'level': 'DEBUG',
-#             'propagate': False,
-#         },
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '[contactor] %(levelname)s %(asctime)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'syslog':{
+            'level':'INFO',
+            'class': 'logging.handlers.SysLogHandler',
+            'facility': SysLogHandler.LOG_LOCAL2,
+            'address': '/dev/log',
+            'formatter': 'verbose',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'syslog', 'mail_admins'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
