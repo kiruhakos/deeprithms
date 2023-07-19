91e56501620c260614d2dc9bad396cd7dcb5c4ef
Kyrylo Kostiuk <110406944+kiruhakos@users.noreply.github.com>
2023-07-19 13:32:36 +0300

    Changed settings.py

--- aiplace/settings.py
+++ aiplace/settings.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 from rest_framework.settings import api_settings
 from decouple import config
 import dj_database_url
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
