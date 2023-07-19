f34bcb7d2ca038ab8f297c605e6c3ea5e983426f
Kyrylo Kostiuk <110406944+kiruhakos@users.noreply.github.com>
2023-07-19 13:38:29 +0300

    Changed settings.py

--- aiplace/settings.py
+++ aiplace/settings.py
@@ -18,6 +18,7 @@ from decouple import config
 import dj_database_url
 import logging
 import logging.handlers
+from logging.handlers import SysLogHandler
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
