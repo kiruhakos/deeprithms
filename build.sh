e8ea7976231a85faf72ada27a5d7abd34681417d
Kyrylo Kostiuk <110406944+kiruhakos@users.noreply.github.com>
2023-09-03 21:06:33 +0300

    Updated build.sh file and made several updates

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -o errexit
 pip install -r requirements.txt
 pip install django-modeltranslation
 
+python manage.py compilemessages -l uk
+
+
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
